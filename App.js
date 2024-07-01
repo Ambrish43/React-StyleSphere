399b28209e4a929337793488037a2b95eebbe766
Ambrish Singh <107296294+Ambrish43@users.noreply.github.com>
2024-07-01 20:17:50 +0530

    Creation of landing page
    this is the 1st step of creating an entire fashion e-commerce website using react

---
+++ App.js
@@ -0,0 +1,33 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <link href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet"/>
+      <header>
+        <h2>STYLE SPHERE</h2>
+      </header>
+      <div className="container">
+        <div className="row1">
+          <p className="i1"> SHOES</p>
+          <p className="i2">BOTTOMS</p>
+        </div>
+
+        <div className="row2">
+          <p className="i1"> TOPS</p>
+          <p className="i2">ACESSORIES</p>
+        </div>
+
+      </div>
+      <footer>
+        <div className="fcontents">
+          <p>Copyright <i class="ri-copyright-line"></i> 2022 Style Sphere. All rights reserved.</p>
+        </div>
+      </footer>
+
+    </div>
+  );
+}
+
+export default App;
